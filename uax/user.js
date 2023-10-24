094dfa8ed9b455ee501abc3690f770d948500d27
badr <badrhamidou@gmail.com>
2023-10-24 18:54:57 +0200

    add style and signup view

---
+++ uax/user.js
@@ -0,0 +1,6 @@
+class user {
+  constructor(nombre, ancho) {
+    this.nombre = nombre;
+    this.ancho = ancho;
+  }
+}
---
+++ user.js
@@ -0,0 +1,5 @@
+import { inputInvalid } from './uax/funcs.js';
+
+let email = document.getElementById("email")
+
+inputInvalid(email)
