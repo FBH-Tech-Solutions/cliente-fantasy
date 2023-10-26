abe09d9a460deb96cca49ae5aa889680a22baf92
badr <badrhamidou@gmail.com>
2023-10-26 21:55:46 +0200

    fix padding h1

--- signup/signup.js
+++ signup/signup.js
@@ -1,10 +1,20 @@
 import { User } from '../uax/user.js';
 
 // nick, name, surnames, email, pass
+let name = document.getElementById("name");
+let surname =  document.getElementById("surname");
+
+let nick = document.getElementById("nick");
+
+let email = document.getElementById("email");
+
+let pass = document.getElementById("pass");
 
 
 if(true){
-    
+
+    let user = new User(nick, name, surnames, email, pass)
+
     let user = new User()
     console.log(user)
 }
