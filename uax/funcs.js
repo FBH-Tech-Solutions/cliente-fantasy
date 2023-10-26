a17ff2ea87240cce6adf7f50cd30d5aaab2bc605
badr <badrhamidou@gmail.com>
2023-10-26 23:16:18 +0200

    add sendNotifications

--- uax/funcs.js
+++ uax/funcs.js
@@ -8,3 +8,19 @@ export function checkByPattern(partternRegex, value){
 
   return partternRegex.test(value)
 }
+
+export function sendNotification(message, type){
+  let newDiv = document.createElement("div")
+  let newP = document.createElement("p")
+  
+  newP.innerHTML =  message
+  newDiv.setAttribute("class", type)
+  newDiv.appendChild(newP)
+  
+  let divNot = document.getElementById("notification")
+  
+  divNot.appendChild(newDiv)
+  
+  document.insertBefore(divNot,newDiv)
+
+}
