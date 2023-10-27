2e36a1a39a3cc9db7f8f55404bcb3c9f001b2ca9
Francisco <coheteomega@gmail.com>
2023-10-27 12:21:44 +0200

    Configuracion datos del usuario

--- uax/componentes.js
+++ uax/componentes.js
@@ -18,26 +18,24 @@ export function navbar(){
       </button>
   
       <!-- Collapsible wrapper -->
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse" id="barraNavegación">
         <!-- Navbar brand -->
         <a class="navbar-brand mt-2 mt-lg-0" href="#">
-          <img
-            src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-            height="15"
-            alt="MDB Logo"
-            loading="lazy"
-          />
+          <img src="../assets/logoFormula1.png" height="20" alt="MDB Logo" loading="lazy"/>
         </a>
         <!-- Left links -->
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" href="#">Dashboard</a>
+            <a class="nav-link" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Team</a>
+            <a class="nav-link" href="#">My drivers</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Projects</a>
+            <a class="nav-link" href="#">Drivers</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Classification</a>
           </li>
         </ul>
         <!-- Left links -->
@@ -88,4 +86,34 @@ export function navbar(){
     </div>
   </nav>`
   document.body.appendChild(newDiv);
+}
+
+export function formProfile(){
+    let newProfile= document.createElement("div")
+
+    newProfile.innerHTML=`<form class="row g-3">
+    <div class="col-md-4">
+  <label for="validationDefault01" class="form-label">Nombre</label>
+  <input type="text" class="form-control" id="name" value="Mark" required>
+</div>
+<div class="col-md-4">
+  <label for="validationDefault02" class="form-label">Apellidos</label>
+  <input type="text" class="form-control" id="surname" value="Otto" required>
+</div>
+<div class="col-md-4">
+  <label for="validationDefaultUsername" class="form-label">Nickname</label>
+  <div class="input-group">
+      <span class="input-group-text" id="nickname">@</span>
+      <input type="text" class="form-control" id="validationDefaultUsername" aria-describedby="inputGroupPrepend2" required>
+    </div>
+</div>
+<div class="col-md-6">
+    <label for="validationDefault03" class="form-label">Email</label>
+    <input type="email" class="form-control" id="email" required>
+</div>
+<div class="col-12">
+    <button class="btn btn-primary" type="submit">Submit form</button>
+</div>
+</form>`
+    document.body.appendChild(newProfile)
 }
