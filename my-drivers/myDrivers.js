38e691359309b529976feba2c41597960a174e46
Francisco <coheteomega@gmail.com>
2023-11-01 14:00:48 +0100

    Funcionalidad de cambiar titular

--- my-drivers/myDrivers.js
+++ my-drivers/myDrivers.js
@@ -11,6 +11,9 @@ var drivers=getLocalStorage('arrDrivers')
 var firstDriver=drivers[4]; //Se tiene que modificar esta parte para que obtengamos los pilotos del usuario
 var secondDriver=drivers[1];
 var button=document.getElementById('submit')
+var optionDriver1=document.getElementById('optionDriver1')
+var optionDriver2=document.getElementById('optionDriver2')
+
 
 function loadDrivers(){
     if (drivers && drivers.length > 1) {
@@ -22,16 +25,15 @@ function loadDrivers(){
         img2.src=secondDriver.img;
         driver2.textContent=secondDriver.name;
         rol2.textContent=secondDriver.rol;
+
+        optionDriver1.textContent=firstDriver.name;
+        optionDriver2.textContent=secondDriver.name;
       } else {
         console.log("We don't found drivers");
       } 
 }
 
-function changeDrivers(){
-    
-}
-
-function changeData(){
+function changeDataDriver1(){
     img1.src=secondDriver.img;
     driver1.textContent=secondDriver.name;
     rol1.textContent=secondDriver.rol;
@@ -41,9 +43,34 @@ function changeData(){
     rol2.textContent=firstDriver.rol;
 }
 
+function changeDataDriver2(){
+    img1.src=firstDriver.img;
+    driver1.textContent=firstDriver.name;
+    rol1.textContent=firstDriver.rol;
+
+    img2.src=secondDriver.img;
+    driver2.textContent=secondDriver.name;
+    rol2.textContent=secondDriver.rol;
+}
+
 button.addEventListener('click', function(){
-    if () {
-        
+    var selectedOption = document.getElementById("inputGroupSelect01").value;
+    if (selectedOption==='1') {
+        if (driver1.textContent==firstDriver.name) {
+            //Error de que el driver es el mismo y debe saltar un mensaje de que es el mismo driver
+            console.log('Este ya es titular')
+        }else{
+            changeDataDriver2()
+        }
+    }else{
+        if (selectedOption==='2') {
+            if (driver2.textContent==firstDriver.name) {
+                //Error de que el driver es el mismo y debe saltar un mensaje de que es el mismo driver
+                console.log('Este ya es titular')
+            }else{
+                changeDataDriver1()
+            }
+        }
     }
 })
 
