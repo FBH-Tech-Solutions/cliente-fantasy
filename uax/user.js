1103cf0815e5313fc3c8a0f41f9bccfe59e3c1a2
Francisco <coheteomega@gmail.com>
2023-11-02 10:02:17 +0100

    Cambio en points de driver

--- uax/user.js
+++ uax/user.js
@@ -1,11 +1,11 @@
 export class User {
-  constructor(nick, name, surnames, email, pass) {
+  constructor(nick, name, surnames, email, pass,points) {
     this.nick = nick;
     this.name = name;
     this.surnames = surnames;
     this.email = email;
     this.pass = pass;
-    this.points=0;
+    this.points=points;
     this.online = 0;
     this.myDrivers = [];
   }
