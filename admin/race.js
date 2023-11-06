b633427e09ec3f29c8ea0cc3e32ab93ac260ce53
badr <badrhamidou@gmail.com>
2023-11-06 19:22:01 +0100

    add storage when finished the race

--- admin/race.js
+++ admin/race.js
@@ -1,27 +1,36 @@
 import { Race } from "../classes/ClassRace.js";
 import { races } from "../utils/default.js"
 import { navbar } from "../utils/componentes.js"
-import { drivers } from "../utils/funcs.js";
+import { setLocalStorate, getLocalStorage, drivers } from "../utils/funcs.js";
 import { getRandomInt } from "../utils/funcs.js"
 
 navbar()
-let arrDrivers = drivers()
-let i = 0
 
+let arrDrivers = null
+let i = 0
 let arrNum = new Array
 
+//Check empty localStorage of drivers
+if(getLocalStorage("drivers")){
+
+    arrDrivers = getLocalStorage("drivers")
+}else{
+    arrDrivers = drivers()
+}
+
+//function to set points random for drivers
 while(i<arrDrivers.length){
     let rand = getRandomInt(arrDrivers.length)
 
     if(arrNum[rand] == null){
         if(i == 0){
-            arrDrivers[rand].points += 30
+            arrDrivers[rand].points += 25
         }else if(i == 1){
-            arrDrivers[rand].points += 20
+            arrDrivers[rand].points += 18
         }else if ( i == 2 ){
-            arrDrivers[rand].points += 10
+            arrDrivers[rand].points += 15
         }else{
-            arrDrivers[rand].points += getRandomInt(9)
+            arrDrivers[rand].points += getRandomInt(7)
         }
 
         arrNum[rand]++
@@ -31,4 +40,7 @@ while(i<arrDrivers.length){
 
     i++
 }
+
+//save the new points
+setLocalStorate("drivers", arrDrivers)
 
