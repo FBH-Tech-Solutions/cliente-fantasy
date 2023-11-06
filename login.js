c3e4b753cfac8424f059f3eb857e55e77dcfc3ca
Francisco <coheteomega@gmail.com>
2023-11-06 11:07:14 +0100

    Correcciones en pequeños errores

--- login.js
+++ login.js
@@ -1,29 +1,31 @@
-import { inputInvalid } from './utils/funcs.js';
+import { getLocalStorage, inputInvalid, sendNotification } from './utils/funcs.js';
 import { checkByPattern } from './utils/funcs.js';
 import { empty } from './utils/funcs.js';
 import { cleanValue } from './utils/funcs.js';
-
+var EXcheckEmail = /^[\w-\.]+@([\w-\.]{3,15})+[\w-]{2,8}$/;
 var EXcheckPassword = /^(?=.*[*#$.])(?!.*\s)[*#$.\w]{6,12}$/;
-var pwd = "admin123";
-var correo = "user@gmail.com";
 
 document.getElementById("submit").addEventListener("click", () => {
-  var email = document.getElementById("email");
+  var email = document.getElementById("email").value;
   var password = document.getElementById("password");
+  var users = getLocalStorage("users");
 
-  if (empty(email) && !checkByPattern(EXcheckEmail, email.value)) {
-    //El correo esta vacio o no esta bien el arroba
-  } else {
-    if (empty(password) && checkByPattern(EXcheckPassword, password.value)) {
-      //La contraseña esta vacia o la contraseña es muy corta
+  for (let i = 0; i < users.length; i++) {
+    var user = users[i];
+    if (empty(email) || !checkByPattern(EXcheckEmail, email)) {
+      // El correo está vacío o no cumple con el patrón
+      sendNotification("Email is empty or not well formed", "alert alert-danger")
+    } else if (empty(password.value) || checkByPattern(EXcheckPassword, password.value)) {
+      // La contraseña está vacía o no cumple con el patrón
+      sendNotification("Password is empty or not well formed", "alert alert-danger")
+      cleanValue(password);
+    } else if (email == user.email && password.value == user.pass) {
+      location.href = "signup/index.html";
+      // Las credenciales coinciden, redirige a la página de inicio de sesión.
     } else {
-      if (password == pwd && email == correo) {
-        location.href = "signup/index.html";
-      } else {
-        //No ha podido iniciar sesion
-        cleanValue(email);
-        cleanValue(password);
-      }
+      // Las credenciales no coinciden, realiza alguna acción como mostrar un mensaje de error.
+      cleanValue(password);
     }
   }
 });
+
