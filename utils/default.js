c41a4d1b5a5a154a4cfe9184a40a33c896b412a3
badr <badrhamidou@gmail.com>
2023-11-07 13:05:37 +0100

    add default for obj

---
+++ utils/default.js
@@ -0,0 +1,13 @@
+import { Race } from "../classes/ClassRace.js";
+export let races = [
+    new Race(1, 'Exciting race through the bustling streets of New York', 'New York', '../assets/races/bahrein.jpg', true),
+    new Race(2, 'Scenic race along the coastline of Los Angeles', 'Los Angeles', '../assets/races/bahrein.jpg', false),
+    new Race(3, 'Thrilling race in the heart of Chicago', 'Chicago', 'chicago.jpg', true),
+    new Race(4, 'Spectacular race along the beaches of Miami', 'Miami', 'miami.jpg', false),
+    new Race(5, 'Iconic race in the picturesque city of San Francisco', 'San Francisco', 'sanfrancisco.jpg', true),
+    new Race(6, 'Glamorous race in the entertainment capital of Las Vegas', 'Las Vegas', 'lasvegas.jpg', false),
+    new Race(7, 'Scenic race through the beautiful landscapes of Seattle', 'Seattle', 'seattle.jpg', true),
+    new Race(8, 'Exciting race at high altitudes in Denver', 'Denver', 'denver.jpg', false),
+    new Race(9, 'Historic race through the streets of Boston', 'Boston', 'boston.jpg', true),
+    new Race(10, 'Challenging race in the vibrant city of Dallas', 'Dallas', 'dallas.jpg', false)
+]
