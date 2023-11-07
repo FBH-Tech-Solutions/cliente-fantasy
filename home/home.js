979f8d4e378cc11fbbddcd64183986bc50e9c9eb
badr <badrhamidou@gmail.com>
2023-11-07 19:55:18 +0100

    add feature charge the news

--- home/home.js
+++ home/home.js
@@ -1,3 +1,25 @@
 import { navbar } from "../utils/componentes.js";
+import { newsDefault } from "../utils/default.js"; 
+import { getLocalStorage } from "../utils/funcs.js";
 
 navbar()
+
+let news = getLocalStorage("news")
+
+if(news == null){
+    news = newsDefault
+}
+
+let sectionNews = document.getElementById("section-news")
+
+news.forEach(element => {
+    console.log(element)
+    sectionNews.innerHTML+=`<div class="card m-3" style="width: 18rem;">
+    <img src="${element.image}" id="test" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${element.title}</h5>
+      <p class="card-text">${element.bodyNew}</p>
+    </div>
+    </div>`    
+});
+
