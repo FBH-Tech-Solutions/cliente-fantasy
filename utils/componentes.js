886065868ab7f4e5ddcc6c14bcb292f5b4444ba5
badr <badrhamidou@gmail.com>
2023-11-07 20:18:39 +0100

    fix bugs

--- utils/componentes.js
+++ utils/componentes.js
@@ -1,92 +1,41 @@
 export function navbar() {
   let newDiv = document.createElement("div");
 
-  newDiv.innerHTML = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <!-- Container wrapper -->
-    <div class="container-fluid">
-      <!-- Toggle button -->
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-mdb-toggle="collapse"
-        data-mdb-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i class="fas fa-bars"></i>
-      </button>
-  
-      <!-- Collapsible wrapper -->
-      <div class="collapse navbar-collapse" id="barraNavegación">
-        <!-- Navbar brand -->
-        <a class="navbar-brand mt-2 mt-lg-0" href="#">
-          <img src="../assets/logoFormula1.png" height="20" alt="MDB Logo" loading="lazy"/>
-        </a>
-        <!-- Left links -->
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">My drivers</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Drivers</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Classification</a>
-          </li>
-        </ul>
-        <!-- Left links -->
-      </div>
-      <!-- Collapsible wrapper -->
-  
-      <!-- Right elements -->
-      <div class="d-flex align-items-center">
-        <!-- Icon -->
-        <a class="text-reset me-3" href="#">
-          <i class="fas fa-shopping-cart"></i>
-        </a>
-
-        <!-- Avatar -->
-        <div class="dropdown">
-          <a
-            class="dropdown-toggle d-flex align-items-center hidden-arrow"
-            href="#"
-            id="navbarDropdownMenuAvatar"
-            role="button"
-            data-mdb-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <img
-              src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
-              class="rounded-circle"
-              height="25"
-              alt="Black and White Portrait of a Man"
-              loading="lazy"
-            />
-          </a>
-          <ul
-            class="dropdown-menu dropdown-menu-end"
-            aria-labelledby="navbarDropdownMenuAvatar"
-          >
-            <li>
-              <a class="dropdown-item" href="#">My profile</a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">Settings</a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">Logout</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </nav>`;
-  let test = document.getElementById("test")
-  test.appendChild(newDiv)
+  newDiv.innerHTML = `	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+	        <div class="container">
+	          <a class="navbar-brand d-md-none d-xs-block py-3" href="#">
+	            <img src="/static_files/images/logos/beer_white.png" height="40" alt="Company Logo">
+	          </a>
+	          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+	            <span class="navbar-toggler-icon"></span>
+	          </button>
+	      
+	          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+	            <ul class="navbar-nav mx-auto">
+	              <li class="nav-item">
+	                <a class="nav-link mx-2 active" aria-current="page" href="/home">Home</a>
+	              </li>
+	              <li class="nav-item">
+	                <a class="nav-link mx-2" href="/drivers">Drivers</a>
+	              </li>
+	              <li class="nav-item">
+	                <a class="nav-link mx-2" href="/my-drivers">My drivers</a>
+	              </li>
+                <li class="nav-item">
+	                <a class="nav-link mx-2" href="/admin">Race</a>
+	              </li>
+	              <li class="nav-item">
+	                <a class="nav-link mx-2" href="/ranking">Ranking</a>
+	              </li>
+	              <li class="nav-item">
+	                <a class="nav-link mx-2 btn rounded-0 btn-danger" href="#">Profile</a>
+	              </li>
+	            </ul>
+	          </div>
+	        </div>
+	      </nav>`;
+  let navbar = document.getElementById("navbar")
+  navbar.appendChild(newDiv)
   // document.body.insertBefore(test, newDiv);
 }
 
