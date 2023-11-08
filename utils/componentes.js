fff9dd9ffcf5415d9edb68d00ccb728b39f1c801
badr <badrhamidou@gmail.com>
2023-11-08 20:43:39 +0100

    fix navbar

--- utils/componentes.js
+++ utils/componentes.js
@@ -1,42 +1,49 @@
-export function navbar() {
+export function navbar(nickname) {
   let newDiv = document.createElement("div");
 
-  newDiv.innerHTML = `	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-	        <div class="container">
-	          <a class="navbar-brand d-md-none d-xs-block py-3" href="#">
-	            <img src="/static_files/images/logos/beer_white.png" height="40" alt="Company Logo">
-	          </a>
-	          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-	            <span class="navbar-toggler-icon"></span>
-	          </button>
-	      
-	          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-	            <ul class="navbar-nav mx-auto">
-	              <li class="nav-item">
-	                <a class="nav-link mx-2 active" aria-current="page" href="/home">Home</a>
-	              </li>
-	              <li class="nav-item">
-	                <a class="nav-link mx-2" href="/drivers">Drivers</a>
-	              </li>
-	              <li class="nav-item">
-	                <a class="nav-link mx-2" href="/my-drivers">My drivers</a>
-	              </li>
-                <li class="nav-item">
-	                <a class="nav-link mx-2" href="/admin">Race</a>
-	              </li>
-	              <li class="nav-item">
-	                <a class="nav-link mx-2" href="/ranking">Ranking</a>
-	              </li>
-	              <li class="nav-item">
-	                <a class="nav-link mx-2 btn rounded-0 btn-danger" href="#">Profile</a>
-	              </li>
-	            </ul>
-	          </div>
-	        </div>
-	      </nav>`;
+  newDiv.innerHTML = `    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <div class="container">
+	<a class="navbar-brand py-3" href="/home">
+	  <img src="/assets/logoFormula1.png" height="40" alt="Company Logo">
+	</a>
+	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+	  <span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarNavDropdown">
+	  <ul class="navbar-nav mx-auto">
+		<li class="nav-item">
+		  <a class="nav-link mx-2 active" aria-current="page" href="/home">Home</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link mx-2" href="/drivers">Drivers</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link mx-2" href="/my-drivers">My drivers</a>
+		</li>
+	  <li class="nav-item">
+		  <a class="nav-link mx-2" href="/admin">Race</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link mx-2" href="/ranking">Ranking</a>
+		</li>
+		<li class="nav-item dropdown">
+		  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+			${nickname}
+		  </a>
+		  <ul class="dropdown-menu dropdown-menu-dark">
+			<li><a class="dropdown-item" href="/profile">My profile</a></li>
+			<li><a class="dropdown-item btn btn-danger" href="/">Logout</a></li>
+		  </ul>
+		</li>
+
+	  </ul>
+	</div>
+  </div>
+</nav>`;
   let navbar = document.getElementById("navbar")
   navbar.appendChild(newDiv)
-  // document.body.insertBefore(test, newDiv);
+
 }
 
 export function formProfile() {
